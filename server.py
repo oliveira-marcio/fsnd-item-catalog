277e8f88cd67d5de3f860b149cd3c3cd1466038b
Márcio Oliveira <emaildomso@gmail.com>
2018-10-30 15:58:23 -0300

    feat: Template adicionado para o catálogo
    A página inicial exibe as categorias e respectivos itens e faz o
    tratamento de categoria inválida passada pela URL. Os dados atuais são
    fake.

--- server.py
+++ server.py
@@ -1,19 +1,69 @@
 #!/usr/bin/env python2
 # coding: utf-8
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+import random, string
 
 app = Flask(__name__)
+
+# Fake itens
+categories = [
+    {
+        "id": 1,
+        "name": "Soccer",
+    },
+    {
+        "id": 2,
+        "name": "Basketball",
+    },
+    {
+        "id": 3,
+        "name": "Volleyball",
+    }
+]
+
+items = [
+    {
+        "id": 1,
+        "title": "Soccer Ball",
+        "description": "It's a ball",
+        "category_id": 1,
+        "user_id": 1
+    },
+    {
+        "id": 2,
+        "title": "Basket",
+        "description": "It's a basket",
+        "category_id": 2,
+        "user_id": 1
+    },
+    {
+        "id": 3,
+        "title": "Shoes",
+        "description": "It's a shoes",
+        "category_id": 1,
+        "user_id": 1
+    }
+]
+
 
 @app.route('/')
 @app.route('/catalog')
 def showCatalog():
-    return "Página inicial com todos as categorias e últimos items."
+    return render_template("catalog.html", categories = categories, items = items, category_name = None)
 
 @app.route('/catalog/<category_name>')
 @app.route('/catalog/<category_name>/items')
 def showAllItems(category_name):
-    return "Items de '%s'" % category_name
+    category = [category for category in categories if category["name"].lower() == category_name.lower()]
+    if not len(category):
+        flash("Category not found")
+        return redirect(url_for('showCatalog'))
+
+    category_id = category[0]["id"]
+    category_name = category[0]["name"]
+    filtered_items = [item for item in items if item["category_id"] == category_id]
+    return render_template("catalog.html", categories = categories, items = filtered_items, category_name = category_name)
 
 @app.route('/catalog/<category_name>/<item_name>')
 def showItem(category_name, item_name):
@@ -52,4 +102,5 @@ def showItemJSON(category_name, item_name):
 
 if __name__ == '__main__':
     app.debug = True
+    app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     app.run(host='0.0.0.0', port=8000)
