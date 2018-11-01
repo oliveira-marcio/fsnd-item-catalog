6793195cbbce426340e37cf6e60d020d8d9dfb00
Márcio Oliveira <emaildomso@gmail.com>
2018-11-01 15:57:31 -0300

    feat: Implementada a API (JSON) para 3 endpoints
    - categorias e respectivos itens
    - itens de uma categoria especificada
    - últimos itens adicionados (com limite de resultados definido na query)

--- models.py
+++ models.py
@@ -7,42 +7,29 @@ Base = declarative_base()
 
 
 class Users(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
-
 class Categories(Base):
-    __tablename__ = 'categories'
+    __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
 
-    @property
-    def serialize(self):
-        return {
-            'name': self.name
-        }
-
 class Items(Base):
-    __tablename__ = 'items'
+    __tablename__ = "items"
 
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     description = Column(String(2000), nullable=False)
-    category_id = Column(Integer, ForeignKey('categories.id'))
+    category_id = Column(Integer, ForeignKey("categories.id"))
     category = relationship(Categories)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship(Users)
 
-    @property
-    def serialize(self):
-        return {
-            'name': self.name
-        }
-
-engine = create_engine('sqlite:///catalog.db', connect_args={'check_same_thread': False})
+engine = create_engine("sqlite:///catalog.db", connect_args={"check_same_thread": False})
 Base.metadata.create_all(engine)
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # coding: utf-8
 
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categories, Items, Users
@@ -9,19 +9,19 @@ import random, string
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///catalog.db',
-                        connect_args={'check_same_thread': False})
+engine = create_engine("sqlite:///catalog.db",
+                        connect_args={"check_same_thread": False})
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-@app.route('/')
-@app.route('/catalog')
+@app.route("/")
+@app.route("/catalog")
 def showCatalog():
     MAX_RESULTS = 10
     categories = session.query(Categories).order_by(Categories.name).all()
-    items = session.query(Items.title, Categories.name.label('category_name')) \
+    items = session.query(Items.title, Categories.name.label("category_name")) \
         .join(Items.category) \
         .order_by(Items.id.desc()) \
         .limit(MAX_RESULTS) \
@@ -29,8 +29,8 @@ def showCatalog():
     return render_template("catalog.html", categories = categories,
                             items = items, category_name = None)
 
-@app.route('/catalog/<category_name>')
-@app.route('/catalog/<category_name>/items')
+@app.route("/catalog/<category_name>")
+@app.route("/catalog/<category_name>/items")
 def showAllItems(category_name):
     categories = session.query(Categories).order_by(Categories.name).all()
     category = session.query(Categories) \
@@ -38,7 +38,7 @@ def showAllItems(category_name):
         .first()
     if not category:
         flash("Category not found")
-        return redirect(url_for('showCatalog'))
+        return redirect(url_for("showCatalog"))
 
     items = session.query(Items) \
         .filter_by(category_id = category.id) \
@@ -47,7 +47,7 @@ def showAllItems(category_name):
     return render_template("catalog.html", categories = categories,
                             items = items, category_name = category.name)
 
-@app.route('/catalog/<category_name>/<item_name>')
+@app.route("/catalog/<category_name>/<item_name>")
 def showItem(category_name, item_name):
     item, return_value = checkCategoryAndItem(category_name, item_name)
 
@@ -57,16 +57,16 @@ def showItem(category_name, item_name):
     else:
         return return_value
 
-@app.route('/catalog/new', defaults={'category_name': None},
+@app.route("/catalog/new", defaults={"category_name": None},
             methods=["GET", "POST"])
-@app.route('/catalog/<category_name>/new', methods=["GET", "POST"])
+@app.route("/catalog/<category_name>/new", methods=["GET", "POST"])
 def addItem(category_name):
-    if request.method == 'POST':
+    if request.method == "POST":
         flash("New item created")
         if category_name:
-            return redirect(url_for('showAllItems', category_name = category_name))
+            return redirect(url_for("showAllItems", category_name = category_name))
         else:
-            return redirect(url_for('showCatalog'))
+            return redirect(url_for("showCatalog"))
 
     categories = session.query(Categories).order_by(Categories.name).all()
 
@@ -75,15 +75,15 @@ def addItem(category_name):
             .filter(Categories.name.ilike(category_name.lower())) \
             .first()
         if not category:
-            return redirect(url_for('addItem'))
+            return redirect(url_for("addItem"))
     return render_template("newitem.html", category_name = category_name,
                             categories = categories)
 
-@app.route('/catalog/<category_name>/<item_name>/edit', methods=["GET", "POST"])
+@app.route("/catalog/<category_name>/<item_name>/edit", methods=["GET", "POST"])
 def editItem(category_name, item_name):
-    if request.method == 'POST':
+    if request.method == "POST":
         flash("Item edited")
-        return redirect(url_for('showItem', category_name = category_name,
+        return redirect(url_for("showItem", category_name = category_name,
                                 item_name = item_name))
 
     item, return_value = checkCategoryAndItem(category_name, item_name)
@@ -95,11 +95,11 @@ def editItem(category_name, item_name):
     else:
         return return_value
 
-@app.route('/catalog/<category_name>/<item_name>/delete', methods=["GET", "POST"])
+@app.route("/catalog/<category_name>/<item_name>/delete", methods=["GET", "POST"])
 def deleteItem(category_name, item_name):
-    if request.method == 'POST':
+    if request.method == "POST":
         flash("Item deleted")
-        return redirect(url_for('showAllItems', category_name = category_name))
+        return redirect(url_for("showAllItems", category_name = category_name))
 
     item, return_value = checkCategoryAndItem(category_name, item_name)
 
@@ -116,7 +116,7 @@ def checkCategoryAndItem(category_name, item_name):
         .first()
     if not category:
         flash("Category not found")
-        return None, redirect(url_for('showCatalog'))
+        return None, redirect(url_for("showCatalog"))
 
     item = session.query(Items) \
         .filter_by(category_id = category.id) \
@@ -125,29 +125,84 @@ def checkCategoryAndItem(category_name, item_name):
 
     if not item:
         flash("Item not found in '%s'" % category.name)
-        return None, redirect(url_for('showAllItems',
+        return None, redirect(url_for("showAllItems",
                                     category_name = category.name.lower()))
 
     return item, None
 
 # Endpoints para a API
 
-@app.route('/catalog.json')
+@app.route("/catalog.json")
 def showCatalogJSON():
-    return "Exibindo todo o catálogo em JSON"
+    categories = session.query(Categories).order_by(Categories.name).all()
 
-@app.route('/catalog/<category_name>.json')
+    categories_list = []
+    for category in categories:
+        categories_list.append(getCategoryEntry(category))
+
+    return jsonify(Categories = categories_list)
+
+@app.route("/catalog/<category_name>.json")
 def showAllItemsJSON(category_name):
-    return "Exibindo Items de '%s' em JSON" % category_name
+    category = session.query(Categories) \
+        .filter(Categories.name.ilike(category_name.lower())) \
+        .first()
+    if not category:
+        return jsonify({"error": "Category not found"})
 
-@app.route('/catalog/<category_name>/<item_name>.json')
-def showItemJSON(category_name, item_name):
-    return "Exibindo item '%s' de '%s' em JSON" % (item_name, category_name)
+    return jsonify(Category = getCategoryEntry(category))
+
+@app.route("/catalog/latest.json")
+def showLatestItemsJSON():
+    max_results = request.args.get("limit")
+    if not max_results:
+        max_results = 10
+
+    items = session.query(Items.title,
+                        Items.description,
+                        Categories.name.label("category_name"),
+                        Users.name.label("creator")) \
+        .join(Items.category, Items.user) \
+        .order_by(Items.id.desc()) \
+        .limit(max_results) \
+        .all()
+
+    items_list = []
+    for i in items:
+        items_list.append({
+            "title": i.title,
+            "description": i.description,
+            "category_name": i.category_name,
+            "creator": i.creator,
+        })
+
+    return jsonify(Latest = items_list)
+
+def getCategoryEntry(category):
+    category_entry = {"name": category.name}
+    items = session.query(Items.title, Items.description, Users.name.label("creator")) \
+        .join(Items.user) \
+        .filter(Items.category_id == category.id) \
+        .order_by(Items.title) \
+        .all()
+
+    if len(items):
+        items_list = []
+        for i in items:
+            items_list.append({
+                "title": i.title,
+                "description": i.description,
+                "creator": i.creator,
+            })
+
+        category_entry["items"] = items_list
+
+    return category_entry
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
-    app.config['SECRET_KEY'] = '' \
+    app.config["SECRET_KEY"] = "" \
         .join(random.choice(string.ascii_uppercase + string.digits)
         for x in xrange(32))
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
